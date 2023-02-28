4f8a803a28d527400b5a7cae5d2374b745258a12
mario <mgabroski@gmail.com>
2023-02-28 10:40:29 +0100

    feat(): Initialize postgreSQL and create class Client

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { connectDB } from "./data-access/connection";
+
+connectDB
+  .initialize()
+  .then(() => {
+    console.log(`Data Source has been initialized`);
+  })
+  .catch((err) => {
+    console.error(`Data Source initialization error`, err);
+  });
